d9e19408f2a0a4f2586d5c3389269cb61cc7aff9
kiratipat <capacity1412@gmail.com>
2023-01-08 13:58:53 +0700

    wrote register_encryption.php

---
+++ register_encryption.php
@@ -0,0 +1,22 @@
+<?php
+include 'conn.php';
+
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+$phone = $_POST['phone'];
+$username = $_POST['username'];
+$password = $_POST['password'];
+// เข้ารหัส password
+$password_hash = hash('sha-512', $password);
+
+$sql = "INSERT INTO member(name,lname,telephone,username,password,status) VALUES ('$fname','$lname','$phone','$username','$password','0')";
+$result = mysqli_query($conn, $sql);
+
+if ($result) {
+    echo "<script>alert('บันทึกข้อมูลเรียบร้อย');</script>";
+    header("location:fr_login.php");
+} else {
+    echo "Error : " . $sql . "<br>" . mysqli_error($conn);
+    echo "<script>alert('ไม่สามารถบันทึกข้อมูลได้');</script>";
+}
+mysqli_close($conn);
